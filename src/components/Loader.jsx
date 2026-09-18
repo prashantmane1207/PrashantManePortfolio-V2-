061c7100823b9e159b513fd3accc8063dbeb3c04
Prashant Mane <prashantmane1207@gmail.com>
2026-09-18 11:51:59 +0530

    Remove developer name from Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -18,9 +18,7 @@ export default function Loader() {
           transition={{ duration: 0.5 }}
         >
           <div className="font-mono text-lg text-text">
-            <span className="text-accent">const</span> developer ={' '}
-            <span className="text-primary">"Prashant"</span>
-            <span className="ml-1 inline-block w-2 h-5 bg-accent animate-blink align-middle" />
+            
           </div>
         </motion.div>
       )}
